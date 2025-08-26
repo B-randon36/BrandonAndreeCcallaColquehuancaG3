2a4dc1abfbe085b450b7d6675ea62d76d134a2e9
nehemiasml <maygamerfernando10@gmail.com>
2025-08-26 08:58:40 -0500

    git poo

--- POO2025-C2G3-Real-main/Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
+++ POO2025-C2G3-Real-main/Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
@@ -1,7 +1,23 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.fxml.FXML;
 import org.springframework.stereotype.Controller;
+
+import java.awt.*;
 
 @Controller
 public class AsistenciaController {
+
+    @FXML
+    TextField txt1,txt2;
+    @FXML
+    Label label1;
+
+    @FXML
+    public void sumar(){
+        double num1 = Double.parseDouble(txt1.getText());
+        double num2 = Double.parseDouble(txt2.getText());
+        double resultado = num1 + num2;
+        txt1.setText(String.valueOf(resultado));
+    }
 }
